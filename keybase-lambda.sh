617cb775e41caa1f34ccc4266260c34738d84c7b
David Meister <thedavidmeister@gmail.com>
2020-08-13 17:21:03 +0400

    jwt check and return all files

--- keybase-lambda.sh
+++ keybase-lambda.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+jwt=$( cat $INPUT_FILE_PATH | jq '.jwt' )
+
+humm_jwt_check --jwt=$jwt --pubkey=$HIVE_PUBKEY
+jwt_valid=$?
+# jwt_valid=0
+if (( $jwt_valid > 0 )) ; then
+ exit $jwt_valid
+fi
+
 ps aux | grep -i /usr/local/bin/kbfsfuse | grep -qv grep
 # if kbfsfuse process exits with a match then grep will have a 0 exit code
 KBFSFUSE_NOT_RUNNING=$?
@@ -21,4 +30,10 @@ if (( $LOGGED_IN == 0 )) ; then
  keybase --no-auto-fork --no-debug oneshot
 fi
 
-keybase fs read keybase://private/thedavidmeister/foo.txt
+# @todo - increase the parallelization in xargs
+# would speed things up for many files but needs better output logic
+# specifically, parallel output to a single file interleaves each line which is
+# very bad obviously
+keybase fs ls -1 -a --rec $HIVE_KEYBASE_PATH | grep .json | xargs -n 1 -P 1 -I "{}" keybase fs read $HIVE_KEYBASE_PATH/{} > items.json
+
+cat items.json | jq -n '.items |= [inputs]'
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 export BUILD=`uuidgen`
-scar init -i thedavidmeister/scarkeybase3 -n $BUILD -e KEYBASE_PAPERKEY="$KEYBASE_PAPERKEY" -e KEYBASE_USERNAME="thedavidmeister"
+scar init -i thedavidmeister/scarkeybase3:latest -n $BUILD -e KEYBASE_PAPERKEY="$KEYBASE_PAPERKEY" -e KEYBASE_USERNAME="thedavidmeister" -e HIVE_KEYBASE_PATH="keybase://team/humm_provenance" -e HIVE_PUBKEY=CSCDkUyH3Ymdzmbcf
+e/OX9EeQg7DvoPRIYWKmcUNKAc=
 scar run -n $BUILD
