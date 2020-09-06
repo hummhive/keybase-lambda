b65ff7d06c25e586bda1814e7a3ce43f62227def
David Meister <thedavidmeister@gmail.com>
2020-09-06 21:35:38 +0400

    working runtime

--- deploy-runtime.sh
+++ deploy-runtime.sh
@@ -12,12 +12,18 @@ fi
 # move into the directory this script is in
 cd "${0%/*}"
 
-docker build -f ./Dockerfile.keybasebuild -t hummhive/keybase-lambda .
-
 mkdir -p build
 mkdir -p runtime
+
+docker build -f ./Dockerfile.keybasebuild -t hummhive/keybase-lambda .
+docker run --rm -v $PWD/build:/build -it hummhive/keybase-lambda
+( cd build && sudo chown -R `whoami`:users . )
+mv -f ./build/* ./runtime
+
 cp ./bootstrap ./runtime/bootstrap
 cd runtime
+ls -la
+chmod +x ./*
 zip runtime.zip ./*
 aws s3 cp ./runtime.zip s3://humm-test/runtime.zip
 aws lambda update-function-code --function-name test-bootstrap --region us-east-1 --s3-bucket humm-test --s3-key runtime.zip
