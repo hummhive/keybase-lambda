9a3e17c3cb18cdaffe3c42178228f12f0bb2cdc0
David Meister <thedavidmeister@gmail.com>
2020-09-08 19:27:38 +0400

    build runtime on ci

---
+++ build-runtime.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -euxo pipefail
+
+# guard against nix shell
+if [[ -z $IN_NIX_SHELL ]]
+ then
+  echo "run needs to be run from the nix shell"
+  exit 1
+fi
+
+# move into the directory this script is in
+cd "${0%/*}"
+
+rm -rf build
+rm -rf runtime
+mkdir -p build
+mkdir -p runtime
+
+docker build -f ./Dockerfile.keybasebuild -t hummhive/keybase-lambda .
+cid=$( docker create hummhive/keybase-lambda )
+docker cp ${cid}:/tmp/build $PWD
+docker rm ${cid}
+
+mv -f ./build/* ./runtime
+
+cp ./bootstrap ./runtime
+
+cd runtime
+chmod +x ./*
+zip runtime.zip ./*
